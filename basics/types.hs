0b9065e5887aa22b888145627659ea32206679c6
asyl <narmatov.asylbek@gmail.com>
2022-06-19 13:33:38 +0600

    defining custom type classes

--- basics/types.hs
+++ basics/types.hs
@@ -5,11 +5,30 @@ class Eq a where
     --- (===), (/==) :: a -> a -> Bool
     (===) :: a -> a -> Bool
     (/==) :: a -> a -> Bool
-
+    x /== y = not (x === y)
 
 
 instance TypeClasses.Eq Bool where
     True === True = True
     False === False = True
     _ === _ = False
-    x /== y = not (x === y)
+
+instance (TypeClasses.Eq a, TypeClasses.Eq b) => TypeClasses.Eq(a, b) where
+    p1 === p2 = fst p1 === fst p2 && snd p1 === snd p2
+
+
+class Printable a where
+    toString :: a -> [Char]
+
+
+instance Printable Bool where
+    toString True = "true"
+    toString False = "false"
+
+
+instance Printable () where
+    toString () = "unit type"
+
+
+instance (Printable a, Printable b) => Printable(a, b) where
+    toString (p1, p2) = "(" ++ toString p1 ++ "," ++ toString p2 ++ ")"
