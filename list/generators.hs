1a7aa402c8316195bdb31ba35e0cfe3c0dfff718
asyl <narmatov.asylbek@gmail.com>
2022-06-25 21:04:34 +0600

    some implementations of list generators

--- list/generators.hs
+++ list/generators.hs
@@ -1,12 +1,23 @@
 module Generator where
-    fib 0 = 0
-    fib 1 = 1
-    fib n = 
-        let
-            iter a b 0 = a
-            iter a b n = iter b (b + a) (n - 1)
-        in
-            iter 0 1 n
+    import Prelude hiding(repeat, replicate, cycle, iterate)
+    f a b = a ++ f b (zipWith (+) a  b)
     
-    infiniteFib n = fib n : [fib (n + 1)]
-    -- fibStream = zipWith fib (\x -> x + 1)
+    fibStream :: [Integer]
+    fibStream = f [0] [1]
+    -- fibStream = 0 : 1 : zipWith (+) fibStream (tail fibStream)
+
+
+    repeat :: a -> [a]
+    repeat x = xs where xs = x : xs
+
+
+    replicate :: Int -> a -> [a]
+    replicate n x = take n (repeat x)
+
+    cycle :: [a] -> [a]
+    cycle [] = error "cycle: Empty list"
+    cycle xs = ys where ys = xs ++ ys
+
+
+    iterate :: (a -> a) -> a -> [a]
+    iterate f x = x : iterate f (f x)
